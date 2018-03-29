19ccbe5204e14a75c455d6e0f8eea7481af3122d
dervishiamela@gmail.com Dervishi <dervishiamela@gmail.com>
2018-03-29 20:07:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 
-if (name=== "Susan")
-{
-  console.log("the name is Susan right?")
-}
-else {
-  condole.log("or is it Joe")
-}
 
 // Don't worry about this
 module.exports = { name, height
